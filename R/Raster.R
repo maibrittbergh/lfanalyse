17a3b67c9919a860b791f144f25728bce33f4367
Mai-Britt Berghoefer <maibrittberghoefer@gmx.de>
2022-12-14 14:54:14 +0100

    include rasterplot and representative Stations

---
+++ R/Raster.R
@@ -0,0 +1,104 @@
+#' Rasterplot of a Discharge Time Series
+#'
+#' @description Function displays discharge Values at a station. Rasterplot covers entire time series
+#'
+
+#' @param data list; Contains all stations that the discharge analysis should consider. List can be created by \link[dischanalyst]{grdc_list}. Each entry of the list contains the existing discharge measurements (as numeric) and the corresponding dates (as character) for the station.
+#' @param station character; Name of the station e.g. "COCHEM" - must be named equally like list entry in data.
+#'@import ggplot2
+#' @return Graphic ; Function visualizes discharge time series as a rasterplot.
+#' @export
+#'
+#' @examples
+#' \dontrun{ Rasterplot("COCHEM", data )}
+Rasterplot=function(station, data){
+
+
+# Creating Dataset --------------------------------------------------------
+
+
+
+  nbr=which(names(data)==station)
+dataset=data[[nbr]]
+
+
+    l= nrow(data[[nbr]])
+    dates=rep(0,l)
+      for (i in 1:  l){
+        dates[i]=as.numeric(substr(data[[nbr]][i,1], 1, 4))
+      }
+
+    dataset=cbind(dataset, dates)
+    month=rep(0,l)
+    for (i in 1:  l){
+      month[i]=as.numeric(substr(data[[nbr]][i,1], 6, 7))
+    }
+
+
+
+    dataset=cbind(dataset, month)
+
+    month_chr=function(datan){
+      l= length(datan)
+    vec=rep(0,l)
+    for ( i in 1:l){
+      if (datan[i]==1) {
+       h="Jan"
+      } else if (datan[i]==2){
+        h="Feb"
+      } else if (datan[i]==3){
+        h="Mar"
+      } else if (datan[i]==4){
+        h="Apr"
+      } else if (datan[i]==5){
+        h="May"
+      } else if (datan[i]==6){
+        h="Jun"
+      } else if (datan[i]==7){
+        h="Jul"
+      }  else if (datan[i]==8){
+        h="Aug"
+      } else if (datan[i]==9){
+        h="Sep"
+      } else if (datan[i]==10){
+        h="Oct"
+      }  else if (datan[i]==11){
+        h="Nov"
+      } else if (datan[i]==12){
+        h="Dec"}
+
+
+
+      vec[i]=h
+    }
+    return(vec)}
+
+      chr=month_chr(dataset$month)
+
+
+dataset=cbind(dataset, chr)
+
+# Statistics of data ------------------------------------------------------
+
+
+Mean=mean(dataset$Value)
+
+
+med=median(dataset$Value)
+
+
+
+# Graphical ---------------------------------------------------------------
+
+titl=paste("Rasterplot of Discharge Time Series at: ", station)
+
+subtitl=paste("from", format(data[[station]][,1], "%Y"),"to", format(data[[station]][(nrow(data[[station]])),1] , "%Y") )[1]
+subtitl
+
+plot=ggplot(dataset, aes(chr, dates )) +
+  geom_raster(aes(fill = Value)) + scale_fill_viridis_c(option = "D")+scale_x_discrete(limit=c("Nov", "Dec", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct"))+ xlab("Month")+ylab("Year")+
+  labs(fill=expression('Discharge Value[m'^3*'/s]'), title = titl, subtitle=subtitl)
+
+return(plot)
+}
+
---
+++ R/metadata_repg.R
@@ -0,0 +1,69 @@
+#' Representative Stations in Germany
+#'
+#'@description Function is searching in input-metadata for representative stations. Depending on the settings, if mark=T it returns the original metadata and adds a column containing logical values. If one entry in the column is TRUE- the station is representative, FALSE if its not. Or, if mark=F it returns a metadata data.frame only containing the representative stations.
+#'
+#' @param metadata "matrix" "array" ;  Metadata of grdc_dataset. Can be created by \link[dischanalyst]{metadata_grdc} function.
+#' @param mark logical; default=F; if FALSE: Function returns new, metadata data.frame containing representative stations only. If TRUE: function returns same metadata data.frame (of GRDC- Dataset) and adds a new column that identifies whether a station is representative (T) or not (F).
+#'
+#' @return "matrix". Same arrangement like metadata. Contains representative stations or marks representative stations in original/input metadata.
+#' @export
+#'
+#' @examples
+#'\dontrun{metadata_repg(metadata_germany)
+#'metadata_germany=metadata_repg(metadata_germany, mark=T)}
+#'
+#'
+
+metadata_repg=function(metadata, mark=F){
+
+  relstat=c("HOHENSAATEN-FINOW", "DRESDEN", "MAGDEBURG-STROMBRUECKE",
+            "RATHENOW UP", "CALBE-GRIZEHNE", "INTSCHEDE",  "HANN.-MUENDEN", "VLOTHO",
+            "VERSEN-WEHRDURCHSTICH", "GREVEN", "MAXAU", "KAUB", "KOELN", "COCHEM", "WUERZBURG" , "ROCKENAU SKA", "ACHLEITEN", "BURGHAUSEN", "WASSERBURG", "LANDSBERG", "KEMPTEN")
+  if(mark==F){
+    #relevante Stationen
+    l=length(relstat)
+    rows=rep(0,l)
+    for ( i in 1:l){
+
+      rows[i]=which(metadata$station==relstat[i])
+    }
+
+
+
+    metadata_repg=metadata[rows,]
+    return(metadata_repg)}
+  #(von Ost nach West):
+  # Ratzdorf / Oder: nicht gefunden  gab es nicht, habe "HOHENSAATEN-FINOW" genommen , oder ist "EISENHUETTENSTADT" besser?
+  #Nienburg / Saale  #Nienburg garb es nicht. Habe CALBE-GRIZEHNE genommen
+  #Höxter; / Weser #höxter gab es nicht, habe "HANN.-MUENDEN", "VLOTHO" genommen
+  #Lingen-Darme / Ems # gab es nicht habe "VERSEN-WEHRDURCHSTICH", "GREVEN" genommen
+
+  #Neu Ulm, Achleiten/ Donau #neu ulm gibts nicht
+
+
+  if(mark==T){
+
+    l=length(relstat)
+    rows=rep(0,l)
+    for ( i in 1:l){
+
+      rows[i]=which(metadata$station==relstat[i])
+    }
+
+
+
+    representative=rep(F,nrow(metadata))
+    representative[rows]=T
+    metadata$rep_stat=representative
+    return(metadata)
+  }
+
+
+
+}
+
+
+
+
+
+
